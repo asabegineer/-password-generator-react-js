5a99c7beeb18068b6fc12d4ae59f912fffca68d2
asabegineer <98164089+asabegineer@users.noreply.github.com>
2023-09-29 22:50:45 +0530

    Update useForm.js

--- useForm.js
+++ useForm.js
@@ -1,1 +1,18 @@
+import { useState } from 'react';
+
+export function useForm(initialValues) {
+  const [values, setValues] = useState(initialValues);
+
+  return [
+    values,
+    (e) => {
+      console.log(e.target.type);
+      setValues({
+        ...values,
+        [e.target.name]:
+          e.target.type === 'checkbox' ? e.target.checked : e.target.value,
+      });
+    },
+  ];
+}
 
